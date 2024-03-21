577296aff6b5f08d8cc6b30a04bb3e0a9b190588
Rob Sheldrick <lebaguettemuncherer@gmail.com>
2024-03-21 15:46:07 +0000

    different meta tag method

--- app/layout.js
+++ app/layout.js
@@ -10,12 +10,12 @@ export const metadata = {
   description: "Full-Stack Developer based in Gloucestershire, UK with a passion for problem solving.",
   keywords: ["Robert Sheldrick", "Rob Sheldrick", "Software Developer", "Full-Stack Developer", "Gloucestershire", "UK", "portfolio", "Developer", "Javascript", "PHP", "React", "CSS"],
   author: "Robert Sheldrick",
-  type: "website",
-  "google-site-verification": "o-Paq6pN2gQHLuHmtCHQPhALcO2PP5bYnRl2AnsUJOU"
+  type: "website"
 };
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="o-Paq6pN2gQHLuHmtCHQPhALcO2PP5bYnRl2AnsUJOU" />
       <Script src="/assets/fontawesome.js" strategy="afterInteractive"/>
       <body className={inter.className}>{children}</body>
     </html>
