b8149056de9672b9925b4ce54f106a3be286dcac
Rob Sheldrick <lebaguettemuncherer@gmail.com>
2024-03-21 16:53:43 +0000

    reverting: testing devicon import in client script

--- app/ScrollAnimation.js
+++ app/ScrollAnimation.js
@@ -1,6 +1,5 @@
 'use client'
 import { useEffect, useState, useRef } from "react";
-import 'devicon/devicon.min.css';
 
 function ScrollAnimation() {
   const [imageLoaded, setImageLoaded] = useState(false);
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from 'next/script';
+import 'devicon/devicon.min.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
